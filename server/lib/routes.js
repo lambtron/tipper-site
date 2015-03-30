50134bce230e319b0a890175b8bb6fbf014470f7
Andy Jiang <andyjiang@gmail.com>
2015-03-30 01:04:58 -0700

    updated server/lib/routes.js

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -5,6 +5,7 @@
 
 var render = require('./render');
 var User = require('./user');
+var Stripe = require('./stripe');
 
 /**
  * Render index html page.
@@ -19,6 +20,12 @@ exports.index = function *() {
  */
 
 exports.register = function *() {
-  var load = this.request.body;
-  return this.body = yield User.create(load.user);
+  var qs = this.request.query;
+  var twitter = qs.state;
+  if (qs.error) return this.body = qs.error_description;
+  var tokens = yield Stripe.auth(qs.code);
+  if (tokens.error) return this.body = tokens.error_description;
+  return this.body = yield User.create(twitter, tokens);
 };
+
+// https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_5xs5jQYK3Jm0RokxGd33Z2LD9Xh79xOF&scope=read_write
