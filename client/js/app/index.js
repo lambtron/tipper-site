88bdd0232517b9831bc3541972fad4fa474f91a4
Andy Jiang <andyjiang@gmail.com>
2015-03-30 01:04:58 -0700

    updated client/js/app/index.js

--- client/js/app/index.js
+++ client/js/app/index.js
@@ -5,6 +5,8 @@
  */
 
 import {component,dom} from '../lib/deku/index.js';
+import Button from '../button/index.js'
+import Input from '../input/index.js'
 
 /**
  * Define `App`.
@@ -23,8 +25,37 @@ export default App;
  */
 
 App.prototype.render = function(props, state) {
+  var self = this;
+  var twitter = '';
+  var stripe = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_5xs5jQYK3Jm0RokxGd33Z2LD9Xh79xOF&scope=read_write&state=';
+
+  // Get twitter.
+  function value(value, name) {
+    twitter = value;
+  }
+
+  // Redirect to stripe oauth.
+  function auth() {
+    console.log(twitter);
+    if (twitter.length === 0) return;
+    window.location = stripe + twitter;
+  }
+
   return (
-    <div>
+    <div class='container' style='width: 500px'>
+      <div style='text-align: center; font-size: 2em; margin-top: 100px'>
+        Meerkash
+      </div>
+      <br />
+      <div>
+        <pre>@andyjiang $20 for the turkey sandwich thanks</pre>
+      </div>
+      <br />
+      <div style='width: 190px; margin: 0 auto'>
+        <Input name='twitter' placeholder='your_twitter' onValid={value} />
+        <br /><br />
+        <img src='../../img/stripe.png' style='cursor: pointer' onClick={auth} />
+      </div>
     </div>
   );
 };
