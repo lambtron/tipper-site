7ff6479d55520cffa6d33b1450639daab40374fa
Andy Jiang <andyjiang@gmail.com>
2015-03-29 23:05:47 -0700

    updated server/lib/user.js

--- server/lib/user.js
+++ server/lib/user.js
@@ -15,10 +15,47 @@ module.exports = User;
 
 /**
  * Upsert user.
+ *
+ * - twitter handle
+ * - stripe accessToken
+ * - stripe requestToken
+ * - paid
+ * - received
+ * - createdAt
  */
 
 User.create = function *(user) {
   var exists = yield this.findOne({ name: user.name });
-  if (!exists) return yield this.insert(user);
-  return 'User already exists';
+  if (!exists) return yield this.insert(newUser(user));
+  return yield this.updateById(exists._id, exists);
 };
+
+/**
+ * New user
+ */
+
+function newUser(user) {
+  return {
+    twitter: user.twitter,
+    stripeAccessToken: user.stripeAccessToken,
+    stripeRequestToken: user.stripeRequestToken,
+    paid: 0,
+    received: 0,
+    createdAt: Date()
+  };
+}
+
+/**
+ * Merge two users, keep some stuff.
+ */
+
+function merge(user, exists) {
+  return {
+    twitter: user.twitter || exists.twitter,
+    stripeAccessToken: user.stripeAccessToken || exists.stripeAccessToken,
+    stripeRequestToken: user.stripeRequestToken || exists.stripeRequestToken,
+    paid: exists.paid,
+    received: exists.received,
+    createdAt: exists.createdAt
+  };
+}
