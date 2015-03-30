5454d1ee9c5b9bef580d201bbb35e64aaf028a62
Andy Jiang <andyjiang@gmail.com>
2015-03-29 23:09:16 -0700

    updated client/js/app/index.js

--- client/js/app/index.js
+++ client/js/app/index.js
@@ -5,16 +5,6 @@
  */
 
 import {component,dom} from '../lib/deku/index.js';
-import Player from '../player/index.js';
-import Button from '../button/index.js';
-import Game from '../game/index.js';
-import List from '../list/index.js';
-
-/**
- * Constants.
- */
-
-const request = require('superagent');
 
 /**
  * Define `App`.
@@ -29,51 +19,12 @@ var App = component();
 export default App;
 
 /**
- * After mount.
- */
-
-App.prototype.afterMount = function(el, props, state) {
-  var setState = this.setState.bind(this);
-  var url = '/api/list';
-
-  // Sort by rating
-  function sortByRating(a, b) {
-    return a.rating > b.rating ? -1 : 1;
-  }
-
-  request.get(url).end(function(err, res) {
-    var list = res.body || [];
-    setState({ list: list.sort(sortByRating) });
-  });
-};
-
-/**
  * Render `App`.
  */
 
 App.prototype.render = function(props, state) {
-  var list = state.list || [];
-
   return (
-    <div style='font-size: 2em'>
-      <div class='container'>
-        <div class='row' style='margin-top: 10px'>
-          <div class='col-xs-12'>
-            <List list={list} />
-          </div>
-        </div>
-      </div>
-      <footer style='position: absolute; bottom: 0px; width: 100%; margin-bottom: 20px;'>
-        <div class='container'>
-          <hr />
-          <div class='row'>
-            <Player visible={true} />
-          </div>
-          <div class='row'>
-            <Game list={list} visible={true} />
-          </div>
-        </div>
-      </footer>
+    <div>
     </div>
   );
 };
