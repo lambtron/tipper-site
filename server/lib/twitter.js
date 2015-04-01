26c894bddec1995509a7a9529049b00a16708344
Andy Jiang <andyjiang@gmail.com>
2015-03-31 23:35:43 -0700

    added server/lib/twitter.js

---
+++ server/lib/twitter.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var thunkify = require('thunkify-wrap');
+var Twitter = require('twitter');
+var ini = require('ini');
+var fs = require('fs');
+
+/**
+ * Static variables.
+ */
+
+var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
+
+/**
+ * Set credentials.
+ */
+
+var twitter = new Twitter({
+  consumer_key: config.TW_CONSUMER_KEY,
+  consumer_secret: config.TW_CONSUMER_SEC,
+  access_token_key: config.TW_ACCESS_TOKEN_KEY,
+  access_token_secret: config.TW_ACCESS_TOKEN_SEC
+});
+
+/**
+ * Thunked get.
+ */
+
+var get = thunkify(twitter.get.bind(twitter));
+
+/**
+ * Expose `twitter`.
+ */
+
+module.exports = twitter;
+
+/**
+ * Look up twitterId from username.
+ */
+
+twitter.getId = function *(username) {
+  var res = yield get('users/show', { screen_name: username, include_entities: false });
+  return res[0].id;
+};
