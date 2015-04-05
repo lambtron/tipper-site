e8d5185b2b76b0063df23bc188b07ae9ef39ced1
Andy Jiang <andyjiang@gmail.com>
2015-04-04 19:08:43 -0700

    updated server/lib/routes.js

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -21,12 +21,12 @@ exports.index = function *() {
  */
 
 exports.register = function *() {
-  // var qs = this.request.query;
-  // var username = qs.state;
-  // var twitterId = yield twitter.getId(username);
-  // var venmo = yield Venmo.auth(qs.code);
-  // if (venmo.error) return this.body = venmo.error;
-  // venmo.twitter = username;
-  // yield User.create(twitterId, venmo);
+  var qs = this.request.query;
+  var username = qs.state;
+  var twitterId = yield twitter.getId(username);
+  var venmo = yield Venmo.auth(qs.code);
+  if (venmo.error) return this.body = venmo.error;
+  venmo.twitter = username;
+  yield User.create(twitterId, venmo);
   return this.body = yield render('success');
 };
