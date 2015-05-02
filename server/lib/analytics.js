1f95ad28a6360393f9012cd264b0e2c0c14594a8
Andy Jiang <andyjiang@gmail.com>
2015-05-02 03:53:41 -0700

    added server/lib/analytics.js

---
+++ server/lib/analytics.js
@@ -0,0 +1,12 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Analytics = require('analytics-node');
+
+/**
+ * Expose `analytics`.
+ */
+
+module.exports = new Analytics('mpvBf2YYAEbw3JnuFX2sx1DpEQ0KMAZO');
